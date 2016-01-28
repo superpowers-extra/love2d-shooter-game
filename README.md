ccb754b2dac18cc7eaece8a46af7aa91ce6e7812
Elisée Maurer <elisee@sparklinlabs.com>
2016-01-28 16:57:32 +0100

    Initial import

---
+++ README.md
@@ -0,0 +1,6 @@
+# LÖVE 2D Demo Shooter
+
+This is the Lua [LÖVE](https://love2d.org/) shooter from [OSM Studios's tutorial](http://osmstudios.com/tutorials/your-first-love2d-game-in-200-lines-part-1-of-3),
+rebuilt with [Superpowers LÖVE](http://github.com/superpowers/superpowers-love2d). 
+
+![](http://i.imgur.com/yLybycP.gif)
---
+++ assets/conf.lua (4)/script.lua
@@ -0,0 +1,10 @@
+-- Configuration
+function love.conf(t)
+	t.title = "Scrolling Shooter Tutorial" -- The title of the window the game is in (string)
+	t.version = "0.10.0"        -- The LÖVE version this game was made for (string)
+	t.window.width = 480        -- we want our game to be long and thin.
+	t.window.height = 800
+
+	-- For Windows debugging
+	t.console = true
+end
